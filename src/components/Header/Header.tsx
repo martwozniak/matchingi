4270c42b7860563df2f649e5bab50390f6a9d82c
Martin <persky03@gmail.com>
2022-12-21 05:16:24 +0100

    Header enhancements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="site__heading flex min-h-[60px] w-full items-center justify-between bg-white px-8">
-      <span className="hover:opacity-85 cursor-pointer select-none">
-        <span className="bold font-bold text-blue-800">match</span>
+    <div className="site__heading flex min-h-[60px] w-full items-center justify-between px-8">
+      <span className="hover:opacity-85 cursor-pointer select-none text-white">
+        <span className="bold font-bold text-yellow-400">match</span>
         ingi
       </span>
 
       <div className="flex gap-4">
-        <span className="hover:opacity-85 cursor-pointer select-none bg-gray-600 px-4 py-2 font-bold text-white">
+        <span className="hover:opacity-85 cursor-pointer select-none rounded-full bg-gray-600/10 px-4 py-2  text-sm font-semibold text-white transition hover:bg-gray-600/20">
           Log in
         </span>
-        <span className="hover:opacity-85 cursor-pointer select-none bg-green-700 px-4 py-2 font-bold text-white">
+        <span className="hover:opacity-85 cursor-pointer select-none  rounded-full bg-yellow-700/10 px-4 py-2 text-sm font-semibold text-white transition hover:bg-yellow-700/20">
           Add job post for 49$
         </span>
       </div>
---
+++ src/pages/order.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Order({}: Props) {
+  return <div>Order job post</div>;
+}
