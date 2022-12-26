8d0cd047d86789595a47ae176348990b7359403b
Martin <persky03@gmail.com>
2022-12-26 23:49:51 +0100

    users

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
 import maplibregl from "maplibre-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-
+import { useRouter } from "next/router";
 //mapboxgl.accessToken = "4lOMEJLMkq3nCLCp0FnT";
 
 export default function Map() {
+  const { locale, locales, defaultLocale } = useRouter();
+
+  let defLng = 139.753;
+  let defLat = 35.6844;
+
+  // if (locale == "en") {
+  //   defLng = 15.55;
+  // }
+
   //const mapContainer = useRef(null);
   //const mapContainer = useRef<HTMLElement | null>(null);
   //const map = useRef(null);
@@ -11,9 +20,9 @@ export default function Map() {
   const mapContainer = useRef<HTMLDivElement | string | any | null>(null);
   // Types TBD
   const map = useRef<maplibregl.Map | null>(null);
-  const [lng] = useState(139.753);
-  const [lat] = useState(35.6844);
-  const [zoom] = useState(14);
+  const [lng] = useState(defLng);
+  const [lat] = useState(defLat);
+  const [zoom] = useState(12);
   const [API_KEY] = useState("4lOMEJLMkq3nCLCp0FnT");
 
   useEffect(() => {
