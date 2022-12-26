3bc8f3841bc44a708b4d61abd4546da6cb540f4c
Martin <persky03@gmail.com>
2022-12-26 19:21:39 +0100

    Menu with programming languages

--- src/components/SelectMenu/SelectMenu.tsx
+++ src/components/SelectMenu/SelectMenu.tsx
@@ -5,21 +5,81 @@ export default function SelectMenu() {
   return (
     <div className="bg-grey-700 min-h-[60px] w-full">
       <div className="flex">
-        <TechnologyCardWithIcon icon="react" text="React.js" />
-        <TechnologyCardWithIcon icon="typescript" text="TypeScript" />
-        <TechnologyCardWithIcon icon="javascript" text="JavaScript" />
-        <TechnologyCardWithIcon icon="python" text="Python" />
-        <TechnologyCardWithIcon icon="java" text="Java" />
-        <TechnologyCardWithIcon icon="php" text="PHP" />
-        <TechnologyCardWithIcon icon="ruby" text="Ruby" />
-        <TechnologyCardWithIcon icon="c" text="C" />
-        <TechnologyCardWithIcon icon="cpp" text="C++" />
-        <TechnologyCardWithIcon icon="dotnet" text=".NET" />
-        <TechnologyCardWithIcon icon="wordpress" text="WordPress" />
-        <TechnologyCardWithIcon icon="unix" text="Admin" />
-        <TechnologyCardWithIcon icon="webflow" text="Webflow" />
-        <TechnologyCardWithIcon icon="putty" text="Admin" />
-        <TechnologyCardWithIcon icon="flutter" text="Mobile" />
+        <TechnologyCardWithIcon
+          icon="react"
+          text="React.js"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="typescript"
+          text="TypeScript"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="javascript"
+          text="JavaScript"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="python"
+          text="Python"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="java"
+          text="Java"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="php"
+          text="PHP"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="ruby"
+          text="Ruby"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="c"
+          text="C"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="cpp"
+          text="C++"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="dotnet"
+          text=".NET"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="wordpress"
+          text="WordPress"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="unix"
+          text="Admin"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="webflow"
+          text="Webflow"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="putty"
+          text="Admin"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
+        <TechnologyCardWithIcon
+          icon="flutter"
+          text="Mobile"
+          description="Programming language with more than 6000 companies using it worldwide"
+        />
       </div>
     </div>
   );
--- src/components/TechnologyCardWithIcon/TechnologyCardWithIcon.tsx
+++ src/components/TechnologyCardWithIcon/TechnologyCardWithIcon.tsx
@@ -1,15 +1,53 @@
 import React from "react";
+import * as HoverCard from "@radix-ui/react-hover-card";
 
 type Props = {
   icon: string;
   text: string;
+  description: string;
 };
 
-export default function TechnologyCardWithIcon({ icon, text }: Props) {
+export default function TechnologyCardWithIcon({
+  icon,
+  text,
+  description,
+}: Props) {
   const iconPath = "/img/icons/" + icon + "/" + icon + "-original.svg";
   return (
     <div className="mx-4 flex cursor-pointer flex-col items-center justify-center gap-1 py-2 text-white hover:opacity-75">
-      <img src={iconPath} alt={text} className="h-[30px] w-[30px]" />
+      <HoverCard.Root>
+        <HoverCard.Trigger asChild>
+          <img src={iconPath} alt={text} className="h-[30px] w-[30px]" />
+        </HoverCard.Trigger>
+        <HoverCard.Portal>
+          <HoverCard.Content className="HoverCardContent" sideOffset={5}>
+            <div className="flex max-w-sm flex-col items-start gap-2 bg-black p-8 text-white">
+              <img className="max-w-[50px]" src={iconPath} alt={text} />
+              <div className="flex flex-col gap-2">
+                <div>
+                  <div className="font-bold">{text}</div>
+                  <div className="cursor-pointer text-blue-600 hover:text-blue-500">
+                    @{text}
+                  </div>
+                </div>
+                <div className="Text">{description}</div>
+                <div style={{ display: "flex", gap: 15 }}>
+                  <div style={{ display: "flex", gap: 5 }}>
+                    <div className="Text bold">0</div>{" "}
+                    <div className="Text faded">Following</div>
+                  </div>
+                  <div style={{ display: "flex", gap: 5 }}>
+                    <div className="Text bold">2,900</div>{" "}
+                    <div className="Text faded">Followers</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <HoverCard.Arrow className="HoverCardArrow" />
+          </HoverCard.Content>
+        </HoverCard.Portal>
+      </HoverCard.Root>
     </div>
   );
 }
