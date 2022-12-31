b55819cd642df0e59642dc76aa78276feccfe891
Martin <persky03@gmail.com>
2022-12-31 14:05:52 +0100

    Rainbow effect logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <div className="site__heading flex min-h-[60px] w-full items-center justify-between bg-white px-8">
       <span className="hover:opacity-85 cursor-pointer select-none">
         <span className="bold font-bold text-blue-800">match</span>
+        {/* TBD: Logo with rainbow effect */}
         ingi
       </span>
 
