032d10fbed1ad358ddfd6d7665f718a3938f5e74
Martin <persky03@gmail.com>
2023-01-02 15:36:41 +0100

    Offes router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,14 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { exampleRouter } from "./example";
+import { offerRouter } from "./offer";
 import { technologiesRouter } from "./technologies";
 
 export const appRouter = router({
   example: exampleRouter,
   auth: authRouter,
   technologies: technologiesRouter,
+  offers: offerRouter,
 });
 
 // export type definition of API
