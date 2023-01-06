102b6d89b876bffc69cc82894d8e722ffd657d40
marcin <persky03@gmail.com>
2023-01-06 22:42:42 +0100

    black bottom CTA

--- src/components/BottomCTA/BottomCTA.tsx
+++ src/components/BottomCTA/BottomCTA.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BottomCTA() {
   return (
-    <div className="absolute bottom-0 flex min-h-[80px] w-full flex-col items-center justify-center gap-1 bg-gray-900 text-white">
+    <div className="absolute backdrop-blur-sm bottom-0 flex min-h-[80px] w-full flex-col items-center justify-center gap-1 bg-black/70 text-white">
       <div className="floating-buy-button-container flex w-full max-w-xl cursor-pointer items-center justify-center rounded-md bg-green-600 py-2 px-8">
         Add job post for 47$
       </div>
