6e9171f558300cd2639a343925fdda3997ee2451
marcin <persky03@gmail.com>
2023-01-07 11:09:50 +0100

    theme

---
+++ src/theme/typography.ts
@@ -0,0 +1,15 @@
+
+export const typography = {
+    /**
+     * The primary font. Used in most places.
+     */
+    primary: "primary",
+    /**
+     * An alternate font used for perhaps titles and stuff.
+     */
+    secondary: "secondary",
+    /**
+     * Lets get fancy with a monospace font!
+     */
+    code: "code",
+}
