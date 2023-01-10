46b4298d2aaba267237db7620da6f01edfdb6058
marcin <persky03@gmail.com>
2023-01-10 21:41:47 +0100

    Modal boilerplate without transitions

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { RxPinRight, RxPlus, RxPaperPlane } from "react-icons/rx";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import LoginModal from "../LoginModal/LoginModal";
 
 export default function Header() {
   const { status } = useSession();
@@ -19,6 +20,7 @@ export default function Header() {
       </Link>
 
       <div className="flex gap-4">
+        {/* 
         <Link href={"/login"}>
           <div className="hover:opacity-85 flex cursor-pointer select-none items-center gap-2 rounded-md bg-gray-600/60 px-4 py-2  text-sm font-semibold text-white transition hover:bg-gray-600/70" onClick={() => {
             if (status === "unauthenticated") {
@@ -30,7 +32,9 @@ export default function Header() {
           >
             <RxPinRight /> <span>Log in</span>
           </div>
-        </Link>
+        </Link>*/}
+        <LoginModal />
+
         <Link href={"/add-offer"}>
           <div className="floating-buy-button-container hover:opacity-85 flex cursor-pointer select-none items-center gap-2 rounded-md  bg-green-600 px-4 py-2 text-sm font-semibold text-white shadow-lg  transition hover:bg-green-500">
             <RxPaperPlane /> {/* RxPlus */}
