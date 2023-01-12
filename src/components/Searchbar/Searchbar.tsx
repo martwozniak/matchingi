d19747fd6a368950a8c4c00e1c4f66e9f776710a
marcin <persky03@gmail.com>
2023-01-13 00:52:03 +0100

    Better UI / Search bar / tech cards

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Searchbar() {
     return (
-        <div className='pr-6 w-full'>
+        <div className='pr-6 w-[50vw] max-w-[50vw]'>
             <input placeholder="AI, Big Data, Python, TypeScript..." type={'text'} name='searchTerm' className='text-white py-2 min-w-[320px] px-4 border border-white/10 rounded-lg justify-center mx-4 flex gap-4 cursor-pointer bg-transparent w-full transition-all items-center' />
         </div>
     )
