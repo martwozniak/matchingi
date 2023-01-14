d65ac50c4b101256a09da10001e4dfa982cbecce
marcin <persky03@gmail.com>
2023-01-14 23:54:26 +0100

    text sx searchbar

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Searchbar() {
     return (
-        <div className='w-full max-w-[50vw] hidden sm:flex'>
+        <div className='w-full max-w-[50vw] min-h-[48px] text-xs hidden sm:flex'>
             <input placeholder="AI, Big Data, Python, TypeScript..." type={'text'} name='searchTerm' className='text-white py-2 sm:min-w-[320px] px-4 border border-white/10 rounded-lg justify-center mx-4 flex gap-4 cursor-pointer bg-transparent w-full transition-all items-center' />
         </div>
     )
