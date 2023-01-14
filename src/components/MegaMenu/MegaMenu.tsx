811229078b84f6f8c66af74a1e16002e1913b7af
marcin <persky03@gmail.com>
2023-01-14 23:51:00 +0100

    hover effect on menu li items

--- src/components/MegaMenu/MegaMenu.tsx
+++ src/components/MegaMenu/MegaMenu.tsx
@@ -5,9 +5,9 @@ export default function MegaMenu() {
     return (
         <div className='text-xs text-white/90'>
             <ul className='flex gap-4'>
-                <li className='cursor-pointer'>Offers</li>
-                <li className='cursor-pointer'>Products</li>
-                <li className='cursor-pointer'>Blog</li>
+                <li className='cursor-pointer transition-all hover:text-green-500'>Offers</li>
+                <li className='cursor-pointer transition-all hover:text-green-500'>Products</li>
+                <li className='cursor-pointer transition-all hover:text-green-500'>Blog</li>
             </ul>
         </div>
     )
